6562f75c8f9162ef407d3fdff78c63d779c91918
SergeyYahdzhyiev <mr.blooods@gmail.com>
2021-02-05 15:18:16 +0400

    added basic styles to answer btns

--- src/components/AnswerButton/answer.button.jsx
+++ src/components/AnswerButton/answer.button.jsx
@@ -1,9 +1,11 @@
+import { capitalize } from '../../plugins';
+
 import styles from './answer.button.module.scss';
 
 export const AnswerButton = ({ text, id }) => {
   return (
     <button className={styles.answer}>
-      <p className={styles.answer_text}>{text}</p>
+      <p className={styles.answer_text}>{capitalize(text)}</p>
     </button>
   );
 };
