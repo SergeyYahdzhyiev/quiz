86d5f90442e1f2fa4e0003c3578ab4eb12ca98f5
SergeyYahdzhyiev <mr.blooods@gmail.com>
2021-02-05 15:21:51 +0400

    added scores to game screen

--- src/pages/GameScreen/game.screen.jsx
+++ src/pages/GameScreen/game.screen.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { Container } from 'react-bootstrap';
 
 import { GameDisplay, Answers } from '../../components';
@@ -5,10 +6,13 @@ import { GameDisplay, Answers } from '../../components';
 import styles from './game.screen.module.scss';
 
 export const GameScreen = () => {
+  const { prizePool, score } = useSelector((state) => state.game);
   return (
     <Container className={styles.game} fluid>
       <GameDisplay />
       <Answers />
+      <h1>Prize Pool: {prizePool}</h1>
+      <h3>Score: {score}</h3>
     </Container>
   );
 };
