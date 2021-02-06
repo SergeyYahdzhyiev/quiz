c3173bfe9fa0f3327478d6c62db1a717cf436b34
SergeyYahdzhyiev <mr.blooods@gmail.com>
2021-02-06 15:43:44 +0400

    developed modal component

--- src/components/TopBar/top.bar.jsx
+++ src/components/TopBar/top.bar.jsx
@@ -1,14 +1,12 @@
 import { Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 
 import { Lives, Skips } from '../';
-import { loseSkip } from '../../redux/actions';
+import { loseSkip, showModal } from '../../redux/actions';
 
 import styles from './top.bar.module.scss';
 
 export const TopBar = () => {
-  const history = useHistory();
   const dispatch = useDispatch();
 
   const { skips } = useSelector((state) => state.game);
@@ -26,7 +24,7 @@ export const TopBar = () => {
         <button onClick={handleSkip} disabled={!skips}>
           Skip Question
         </button>
-        <button onClick={() => history.push('/end')}>End Game</button>
+        <button onClick={() => dispatch(showModal())}>End Game</button>
       </Row>
     </Row>
   );
--- src/pages/GameScreen/game.screen.jsx
+++ src/pages/GameScreen/game.screen.jsx
@@ -1,14 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Container, Row } from 'react-bootstrap';
 import { useHistory } from 'react-router';
+import { CSSTransition } from 'react-transition-group';
 
-import { GameDisplay, Answers, TopBar } from '../../components';
-import { endGame } from '../../redux/actions';
+import { GameDisplay, Answers, TopBar, Modal } from '../../components';
+import { endGame, hideModal } from '../../redux/actions';
 
 import styles from './game.screen.module.scss';
 
 export const GameScreen = () => {
   const { prizePool, score, lives } = useSelector((state) => state.game);
+  const { show } = useSelector((state) => state.modal);
 
   const history = useHistory();
   const dispatch = useDispatch();
@@ -20,8 +22,20 @@ export const GameScreen = () => {
     history.push('/lose');
   }
 
+  const handleEnd = () => {
+    dispatch(hideModal());
+    dispatch(endGame());
+    history.push('/themes');
+  };
+
   return (
     <Container className={styles.game} fluid>
+      <CSSTransition in={show} classNames='modals' timeout={500} unmountOnExit>
+        <Modal
+          message='Are you sure you want to end the game?'
+          onConfirm={handleEnd}
+        />
+      </CSSTransition>
       <TopBar />
       <GameDisplay />
       <Answers />
